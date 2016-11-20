8a415dd7ee9af8f8c293e33b082405cc2f89bc49
Stephane Reynders <spoutnik@gmail.com>
2016-11-20 16:57:05 +0100

    Crud actions btn-xs

--- resources/views/includes/crud/actions.blade.php
+++ resources/views/includes/crud/actions.blade.php
@@ -1,7 +1,7 @@
-{{--<a href="{{ route(sprintf('%s.show', $resourcePrefix), [$object->pk]) }}" class="btn btn-default btn-sm"><i class="fa fa-eye"></i> {{ __('View') }}</a>--}}
-<a href="{{ route(sprintf('%s.edit', $resourcePrefix), [$object->pk]) }}" class="btn btn-default btn-sm"><i class="fa fa-edit"></i> {{ __('Edit') }}</a>
+{{--<a href="{{ route(sprintf('%s.show', $resourcePrefix), [$object->pk]) }}" class="btn btn-default btn-xs"><i class="fa fa-eye"></i> {{ __('View') }}</a>--}}
+<a href="{{ route(sprintf('%s.edit', $resourcePrefix), [$object->pk]) }}" class="btn btn-default btn-xs"><i class="fa fa-edit"></i> {{ __('Edit') }}</a>
 <form action="{{ route(sprintf('%s.destroy', $resourcePrefix), [$object->pk]) }}" method="post" style="display: inline;" onsubmit="return confirm('{{ __("Are your sure?") }}');">
     {{ method_field('delete') }}
     {{ csrf_field() }}
-    <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i> {{ __('Delete') }}</button>
+    <button type="submit" class="btn btn-danger btn-xs"><i class="fa fa-trash"></i> {{ __('Delete') }}</button>
 </form>
