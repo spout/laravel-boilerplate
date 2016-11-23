9fce11cb50610a6c9756db432ef6be04ae34aa68
Stephane Reynders <spoutnik@gmail.com>
2016-11-23 19:35:16 +0100

    Fix viewPath with locale prefix

--- app/Http/Controllers/Traits/Crud/CommonTrait.php
+++ app/Http/Controllers/Traits/Crud/CommonTrait.php
@@ -22,6 +22,7 @@ trait CommonTrait
         $path = strtolower(str_replace('Controller', '', class_basename(static::class)));
         $prefix = request()->route()->getPrefix();
         if (!empty($prefix)) {
+            list($locale, $prefix) = explode('/', $prefix);
             $path = sprintf('%s.%s', $prefix, $path);
         }
         return $path;
