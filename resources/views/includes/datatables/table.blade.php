8b75a70705b58fb4a055999129f6974132b7ee81
Stephane Reynders <spoutnik@gmail.com>
2017-02-19 10:37:20 +0100

    Add datatables language

--- resources/views/includes/datatables/table.blade.php
+++ resources/views/includes/datatables/table.blade.php
@@ -55,7 +55,31 @@
             processing: true,
             serverSide: true,
             ajax: '{!! $ajax !!}',
-            columns: {!! $datatableColumns !!}
+            columns: {!! $datatableColumns !!},
+            "language": {
+                "emptyTable":     "{{ __("No data available in table") }}",
+                "info":           "{{ __("Showing _START_ to _END_ of _TOTAL_ entries") }}",
+                "infoEmpty":      "{{ __("Showing 0 to 0 of 0 entries") }}",
+                "infoFiltered":   "{{ __("(filtered from _MAX_ total entries)") }}",
+                "infoPostFix":    "",
+                "thousands":      ",",
+                "lengthMenu":     "{{ __("Show _MENU_ entries") }}",
+                "loadingRecords": "{{ __("Loading...") }}",
+                "processing":     "{{ __("Processing...") }}",
+                "search":         "_INPUT_", //Search:
+                "zeroRecords":    "{{ __("No matching records found") }}",
+                "paginate": {
+                "first":      "{{ __("First") }}",
+                    "last":       "{{ __("Last") }}",
+                    "next":       "{{ __("Next") }}",
+                    "previous":   "{{ __("Previous") }}"
+                },
+                "aria": {
+                    "sortAscending":  "{{ __(": activate to sort column ascending") }}",
+                        "sortDescending": "{{ __(": activate to sort column descending") }}"
+                },
+                "searchPlaceholder": "{{ __("Quick search...") }}"
+            }
         });
     });
 </script>
