10e9ae5866e0108231c7cf187d03d8c7a3e5412f
Stephane Reynders <spoutnik@gmail.com>
2017-02-20 19:01:21 +0100

    Datatables action btn-group

--- resources/views/includes/datatables/action.blade.php
+++ resources/views/includes/datatables/action.blade.php
@@ -1,7 +1,13 @@
-{{--<a href="{{ route(sprintf('%s.show', $resourcePrefix), [$object->pk]) }}" class="btn btn-default btn-xs"><i class="fa fa-eye"></i> {{ __('View') }}</a>--}}
-<a href="{{ route(sprintf('%s.edit', $resourcePrefix), [$object->pk]) }}" class="btn btn-default btn-xs"><i class="fa fa-edit"></i> {{ __('Edit') }}</a>
-<form action="{{ route(sprintf('%s.destroy', $resourcePrefix), [$object->pk]) }}" method="post" style="display: inline;" onsubmit="return confirm('{{ __("Are your sure?") }}');">
-    {{ method_field('delete') }}
-    {{ csrf_field() }}
-    <button type="submit" class="btn btn-danger btn-xs"><i class="fa fa-trash"></i> {{ __('Delete') }}</button>
-</form>
+<div class="btn-group btn-group-xs">
+    {{--<a href="{{ route(sprintf('%s.show', $resourcePrefix), [$object->pk]) }}" class="btn btn-default btn-xs"><i class="fa fa-eye"></i> {{ __('View') }}</a>--}}
+    <a href="{{ route(sprintf('%s.edit', $resourcePrefix), [$object->pk]) }}" class="btn btn-default btn-xs"><i class="fa fa-edit"></i> {{ __('Edit') }}</a>
+    <?php
+    $route = ["$resourcePrefix.destroy", $object->pk];
+    $formName = 'post_' . md5(json_encode($route));
+    $confirm = __('Are you sure?');
+    $onclick = "if (confirm('$confirm')) { document.$formName.submit(); } event.returnValue = false; return false;";
+    ?>
+    <a href="#" onclick="{!! $onclick !!}" class="btn btn-danger"><i class="fa fa-trash"></i> {{ __('Delete') }}</a>
+    {{ Form::open(['route' => $route, 'method' => 'delete', 'name' => $formName]) }}
+    {{ Form::close() }}
+</div>
