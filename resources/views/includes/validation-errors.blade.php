19b2ee1ad23dda5de20fede17457ae41ba392dbd
Stephane Reynders <spoutnik@gmail.com>
2017-05-07 17:45:15 +0200

    Validation errors message

--- resources/views/includes/validation-errors.blade.php
+++ resources/views/includes/validation-errors.blade.php
@@ -1,5 +1,6 @@
 @if (count($errors) > 0)
     <div class="alert alert-danger alert-important">
+        <p>{{ _i("Please correct the errors below:") }}</p>
         <ul>
             @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
