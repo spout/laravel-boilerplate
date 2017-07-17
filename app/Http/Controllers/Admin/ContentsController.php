1c003eca1dd6aced7bfeb6dcbb111b6f67938c96
Stephane Reynders <spoutnik@gmail.com>
2017-07-17 19:01:30 +0200

    Add ContentFormRequest

--- app/Http/Controllers/Admin/ContentsController.php
+++ app/Http/Controllers/Admin/ContentsController.php
@@ -2,10 +2,12 @@
 namespace App\Http\Controllers\Admin;
 
 use App\DataTables\ContentsDataTable;
+use App\Http\Requests\ContentFormRequest;
 use App\Models\Content;
 
 class ContentsController extends AdminController
 {
     protected static $model = Content::class;
+    protected static $requestClass = ContentFormRequest::class;
     protected static $dataTableClass = ContentsDataTable::class;
 }
