e2743edc1b4f4fa182ddaad1443ccff843d5e753
Stephane Reynders <spoutnik@gmail.com>
2017-07-22 20:51:49 +0200

    Snippets datatable

--- app/Http/Controllers/Admin/SnippetsController.php
+++ app/Http/Controllers/Admin/SnippetsController.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Http\Controllers\Admin;
 
-use App\DataTables\SettingsDataTable;
+use App\DataTables\SnippetsDataTable;
 use App\Http\Requests\SnippetFormRequest;
 use App\Models\Snippet;
 
@@ -9,5 +9,5 @@ class SnippetsController extends AdminController
 {
     protected static $model = Snippet::class;
     protected static $requestClass = SnippetFormRequest::class;
-    protected static $dataTableClass = SettingsDataTable::class;
+    protected static $dataTableClass = SnippetsDataTable::class;
 }
