a6ab7714c654889d640cc5d85879082ed89947fd
Stephane Reynders <spoutnik@gmail.com>
2017-09-01 13:18:41 +0200

    Add Google place autocomplete partial

---
+++ resources/views/includes/google-place-autocomplete.blade.php
@@ -0,0 +1,73 @@
+@push('scripts')
+<script src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_API_KEY') }}&amp;libraries=places"></script>
+<script>
+  'use strict';
+
+  var map, marker, autocomplete;
+
+  google.maps.event.addDomListener(window, 'load', initialize);
+
+  function initialize() {
+    var mapDiv = document.getElementById('map-canvas');
+    var lat = $('#lat').val() || 45.8916459;
+    var lng = $('#lng').val() || 6.092474799999991;
+    var latLng = new google.maps.LatLng(lat, lng);
+
+    map = new google.maps.Map(mapDiv, {
+      center: latLng,
+      zoom: 13
+    });
+
+    autocomplete = new google.maps.places.Autocomplete((document.getElementById('{{ $domId }}')), {types: ['geocode']});
+
+    marker = new google.maps.Marker({
+      map: map,
+      position: latLng
+    });
+
+    autocomplete.addListener('place_changed', function() {
+      marker.setVisible(false);
+      var place = autocomplete.getPlace();
+      if (!place.geometry) {
+        window.alert("Autocomplete's returned place contains no geometry");
+        return;
+      }
+
+      if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+      } else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(17);
+      }
+      marker.setPosition(place.geometry.location);
+      marker.setVisible(true);
+
+      updateLocationFields(place.geometry.location.lat(), place.geometry.location.lng(), place.address_components);
+    });
+  }
+
+  function addressComponent(address_components, addressType, nameType) {
+    nameType = nameType || 'long_name';
+
+    for (var i = 0; i < address_components.length; i++) {
+      for (var j = 0; j < address_components[i].types.length; j++) {
+        if (address_components[i].types[j] == addressType) {
+          if (address_components[i][nameType]) {
+            return address_components[i][nameType];
+          }
+        }
+      }
+    }
+  }
+
+  function updateLocationFields(lat, lng, address_components) {
+    //var sublocality = addressComponent(address_components, 'sublocality');
+    //var locality = addressComponent(address_components, 'locality');
+    //var city = sublocality || locality;
+
+    $('#lat').val(lat);
+    $('#lng').val(lng);
+    //$('#city').val(city);
+  }
+</script>
+@endpush
