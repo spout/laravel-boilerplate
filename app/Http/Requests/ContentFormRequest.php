d1a62608b646bceebf373479b83cbeda4788bce1
Stephane Reynders <spoutnik@gmail.com>
2017-10-04 21:51:10 +0200

    Improve content validation

--- app/Http/Requests/ContentFormRequest.php
+++ app/Http/Requests/ContentFormRequest.php
@@ -6,6 +6,12 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class ContentFormRequest extends FormRequest
 {
+    protected $fields = [
+        'title' => 'required',
+        //'slug' => 'required',
+        'path' => 'required',
+    ];
+
     /**
      * Determine if the user is authorized to make this request.
      *
@@ -23,10 +29,43 @@ class ContentFormRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-            'title' => 'required',
-            'slug' => 'required',
-            'content' => 'required',
-        ];
+        $rules = [];
+        $locales = \Config::get('app.locales');
+
+        foreach ($this->fields as $field => $rule) {
+            foreach ($locales as $lang => $locale) {
+                $rules["{$field}_{$lang}"] = $rule;
+            }
+        }
+
+        return $rules;
+    }
+
+    public function messages()
+    {
+        $messages = [];
+        $locales = \Config::get('app.locales');
+
+        foreach ($this->fields as $field => $rule) {
+            switch ($field) {
+                case 'title':
+                    $message = _i("The title field is required.");
+                    break;
+
+                case 'path':
+                    $message = _i("The path field is required.");
+                    break;
+
+                default:
+                    $message = _i("This field is required.");
+                    break;
+            }
+
+            foreach ($locales as $lang => $locale) {
+                $messages["{$field}_{$lang}.required"] = $message;
+            }
+        }
+
+        return $messages;
     }
 }
