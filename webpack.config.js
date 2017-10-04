fcf73be011da4d06b11c802844513ece7cba958a
Stephane Reynders <spoutnik@gmail.com>
2017-10-04 18:33:42 +0200

    Webpack standardjs lint

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,68 @@
 /* global require */
 /* global __dirname */
 
-var webpack = require('webpack');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+require('webpack')
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-    entry: {
-        app: __dirname + "/resources/assets/js/app.js",
-        admin: __dirname + "/resources/assets/js/admin.js"
-    },
-    output: {
-        path: __dirname + '/public/build',
-        filename: "[name].js",
-        chunkFilename: "[id].js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: "css-loader"
-                })
-            },
-            {
-                test: /\.scss$/,
-                loader: ExtractTextPlugin.extract({
-                    loader: [{
-                        loader: "css-loader"
-                    }, {
-                        loader: "sass-loader"
-                    }],
-                    // use style-loader in development
-                    fallbackLoader: "style-loader"
-                })
-            },
-            {
-                test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "url-loader?limit=10000&mimetype=application/font-woff"
-            },
-            {
-                test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "url-loader?limit=10000&mimetype=application/font-woff"
-            },
-            {
-                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "url-loader?limit=10000&mimetype=application/octet-stream"
-            },
-            {
-                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "file-loader"
-            },
-            {
-                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "url-loader?limit=10000&mimetype=image/svg+xml"
-            },
-            {
-                test: /\.(ico|jpe?g|png|gif)$/,
-                loader: "file-loader"
-            }
-        ]
-    },
-    plugins: [
-        new ExtractTextPlugin('[name].css')
+  entry: {
+    app: path.join(__dirname, 'resources/assets/js/app.js'),
+    admin: path.join(__dirname, 'resources/assets/js/admin.js')
+  },
+  output: {
+    path: path.join(__dirname, 'public/build'),
+    filename: '[name].js',
+    chunkFilename: '[id].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract({
+          loader: [{
+            loader: 'css-loader'
+          }, {
+            loader: 'sass-loader'
+          }],
+          // use style-loader in development
+          fallbackLoader: 'style-loader'
+        })
+      },
+      {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+      },
+      {
+        test: /\.(ico|jpe?g|png|gif)$/,
+        loader: 'file-loader'
+      }
     ]
-};
+  },
+  plugins: [
+    new ExtractTextPlugin('[name].css')
+  ]
+}
