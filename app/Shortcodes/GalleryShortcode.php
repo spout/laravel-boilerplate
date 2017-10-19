1bd877f19e17d018f696ff012316c5efdb7efc67
Stephane Reynders <spoutnik@gmail.com>
2017-10-19 23:18:38 +0200

    Fix gallery shortcode columns

--- app/Shortcodes/GalleryShortcode.php
+++ app/Shortcodes/GalleryShortcode.php
@@ -17,7 +17,10 @@ class GalleryShortcode
     {
         $items = glob(public_path($shortcode->directory) . '/*.{jpg,png,gif}', GLOB_BRACE);
         $totalColumns = static::$config['columns']['total'];
-        $columns = empty($shortcode->columns) || ($totalColumns % $shortcode->columns) ? static::$config['columns']['default'] : $shortcode->columns;
+        $columns = $shortcode->columns ?? static::$config['columns']['default'];
+        if ($totalColumns % $columns) {
+            $columns = static::$config['columns']['default'];
+        }
         $template = $shortcode->template ?? static::$config['template']['default'];
         return view('shortcodes.gallery', compact('shortcode', 'items', 'totalColumns', 'columns', 'template'));
     }
