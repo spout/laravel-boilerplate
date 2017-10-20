f0aa05104e700c502b8b94a41f6cd18d523d4af0
Stephane Reynders <spoutnik@gmail.com>
2017-10-20 19:10:14 +0200

    Add fields to contact datatable

--- app/DataTables/ContactsDataTable.php
+++ app/DataTables/ContactsDataTable.php
@@ -18,6 +18,7 @@ class ContactsDataTable extends DataTable
     {
         return [
             ['data' => 'id', 'name' => 'id', 'title' => _i("ID"), 'searchable' => false],
+            ['data' => 'company', 'name' => 'company', 'title' => _i("Company")],
             ['data' => 'email', 'name' => 'email', 'title' => _i("Email")],
             ['data' => 'subject', 'name' => 'subject', 'title' => _i("Subject")],
             ['data' => 'created_at', 'name' => 'created_at', 'title' => _i("Created"), 'searchable' => false],
