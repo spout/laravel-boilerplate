3478a6ebdb7dc4a1b1a514792b1c7fa04c5d1b8d
Stephane Reynders <spoutnik@gmail.com>
2017-10-20 16:03:19 +0200

    Add component in snippets datatables

--- app/DataTables/SnippetsDataTable.php
+++ app/DataTables/SnippetsDataTable.php
@@ -19,6 +19,7 @@ class SnippetsDataTable extends DataTable
         return [
             ['data' => 'slug', 'name' => 'slug', 'title' => _i("Slug")],
             ['data' => 'title', 'name' => 'title', 'title' => _i("Title")],
+            ['data' => 'component', 'name' => 'component', 'title' => _i("Component")],
         ];
     }
 }
