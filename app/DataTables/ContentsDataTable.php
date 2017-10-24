4fe55df69f42fdf604a230fcffe2e7da38fcf0ca
Stephane Reynders <spoutnik@gmail.com>
2017-10-24 19:09:42 +0200

    Remove content datatable columns

--- app/DataTables/ContentsDataTable.php
+++ app/DataTables/ContentsDataTable.php
@@ -17,12 +17,8 @@ class ContentsDataTable extends DataTable
     protected function getColumns()
     {
         return [
-            ['data' => 'id', 'name' => 'id', 'title' => _i("ID"), 'searchable' => false],
             ['data' => 'title', 'name' => 'title', 'title' => _i("Title")],
-            //['data' => 'slug', 'name' => 'slug', 'title' => _i("Slug")],
             ['data' => 'path', 'name' => 'path', 'title' => _i("Path")],
-            //['data' => 'created_at', 'name' => 'created_at', 'title' => _i("Created"), 'searchable' => false],
-            //['data' => 'updated_at', 'name' => 'updated_at', 'title' => _i("Updated"), 'searchable' => false],
         ];
     }
 
