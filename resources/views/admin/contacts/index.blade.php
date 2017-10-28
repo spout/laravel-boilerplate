eca5bb0d09d75b09b3142f80bf5415b834745869
Stephane Reynders <spoutnik@gmail.com>
2017-10-28 15:23:51 +0200

    Fix contact create route

--- resources/views/admin/contacts/index.blade.php
+++ resources/views/admin/contacts/index.blade.php
@@ -3,6 +3,6 @@
 @section('title', _i("Contacts"))
 
 @section('content')
-    @include('includes.admin.create-button', ['url' => route('admin.categories.create')])
+    @include('includes.admin.create-button', ['url' => route('admin.contacts.create')])
     @include('includes.datatables.table')
 @endsection
