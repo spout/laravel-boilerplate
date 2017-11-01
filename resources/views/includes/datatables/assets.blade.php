12d14efec1130f8f998b9ea42b54d79705ce3377
Stephane Reynders <spoutnik@gmail.com>
2017-11-01 19:05:04 +0100

    Fix datatables assets styles => link

--- resources/views/includes/datatables/assets.blade.php
+++ resources/views/includes/datatables/assets.blade.php
@@ -1,10 +1,13 @@
 @push('scripts')
     <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
+    {{--<script src="https://cdn.datatables.net/buttons/1.0.3/js/dataTables.buttons.min.js"></script>--}}
+    {{--<script src="/vendor/datatables/buttons.server-side.js"></script>--}}
     @if (isset($dataTable))
         {!! $dataTable->scripts() !!}
     @endif
 @endpush
 @push('styles')
-    <style href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" rel="stylesheet"></style>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css">
+    {{--<link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.0.3/css/buttons.dataTables.min.css">--}}
 @endpush
