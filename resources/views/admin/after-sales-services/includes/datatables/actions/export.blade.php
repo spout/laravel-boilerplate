e3e693c7d5a7380df0154491f19dbbaa8f61e6e6
Stephane Reynders <spoutnik@gmail.com>
2017-11-02 01:00:31 +0100

    Remove PDF format (not working)

--- resources/views/admin/after-sales-services/includes/datatables/actions/export.blade.php
+++ resources/views/admin/after-sales-services/includes/datatables/actions/export.blade.php
@@ -1,4 +1,4 @@
 @if (in_array('export', $actions) || array_key_exists('export', $actions))
-    <a href="{{ route("$resourcePrefix.export", ['pk' => $object->pk, 'format' => 'pdf']) }}" class="btn btn-default btn-xs"><i class="fa fa-file-pdf-o"></i> {{ _i('PDF') }}</a>
+    {{--<a href="{{ route("$resourcePrefix.export", ['pk' => $object->pk, 'format' => 'pdf']) }}" class="btn btn-default btn-xs"><i class="fa fa-file-pdf-o"></i> {{ _i('PDF') }}</a>--}}
     <a href="{{ route("$resourcePrefix.export", ['pk' => $object->pk, 'format' => 'xlsx']) }}" class="btn btn-default btn-xs"><i class="fa fa-file-excel-o"></i> {{ _i('Excel') }}</a>
 @endif
