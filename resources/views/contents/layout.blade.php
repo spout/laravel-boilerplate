6081783f2ccc64c7682fd5c7518be856bfe65d8f
Stephane Reynders <spoutnik@gmail.com>
2017-11-14 20:57:56 +0100

    Improve contents views

--- resources/views/contents/layout.blade.php
+++ resources/views/contents/layout.blade.php
@@ -1,1 +1,11 @@
 @extends('layouts.app')
+
+@section('content')
+    @if ($content->path !== 'homepage')
+        <div class="container">
+            @yield('content')
+        </div>
+    @else
+        @yield('content')
+    @endif
+@overwrite
--- resources/views/contents/show.blade.php
+++ resources/views/contents/show.blade.php
@@ -9,11 +9,14 @@
             <li><a href="{{ $ancestor->absoluteUrl }}">{{ $ancestor->title }}</a></li>
         @endforeach
     @endif
-    <li><a href="{{ $content->absoluteUrl }}">{{ $content->title }}</a></li>
+    <li class="active">{{ $content->title }}</li>
 @endsection
 
 @section('content')
-    <div>
+    <div id="content-{{ str_replace('/', '-', $content->path) }}">
+        @if (Auth::check() && Auth::user()->is_admin)
+            <div class="text-right"><a href="{{ route('admin.contents.edit', [$content->pk]) }}" class="btn btn-default btn-xs" title="{{ _i("Edit content") }}" data-edit-target="#content-{{ str_replace('/', '-', $content->path) }}">{{ _i("Edit") }}</a></div>
+        @endif
         {!! Shortcode::compile($content->content) !!}
     </div>
 @endsection
