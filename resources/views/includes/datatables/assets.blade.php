0d5bdc846c4be6e7233019bb0f5730a0e1f6ca40
Stephane Reynders <spoutnik@gmail.com>
2018-01-07 15:59:45 +0100

    remove custom dataTables css

--- resources/views/includes/datatables/assets.blade.php
+++ resources/views/includes/datatables/assets.blade.php
@@ -8,7 +8,6 @@
     @endif
 @endpush
 @push('styles')
-    {{--<link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap4.min.css">--}}
-    <link rel="stylesheet" href="{{ asset('css/dataTables.bootstrap4.css') }}">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap4.min.css">
     {{--<link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.5.1/css/buttons.dataTables.min.css">--}}
 @endpush
