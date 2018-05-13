ba82eebdf475cc2c1291bb8d1bac1f0175ffc4b9
Stephane Reynders <spoutnik@gmail.com>
2018-05-13 15:53:43 +0200

    Fix ProductsDataTable slug

--- app/DataTables/ProductsDataTable.php
+++ app/DataTables/ProductsDataTable.php
@@ -13,7 +13,7 @@ class ProductsDataTable extends DataTable
         return [
             ['data' => 'title', 'name' => 'title', 'title' => _i("Title")],
             ['data' => "category.title_plural_{$locale}", 'name' => "category.title_plural_{$locale}", 'title' => _i("Category")],
-            ['data' => 'template.title', 'name' => 'template.title', 'title' => _i("Template")],
+            ['data' => 'template.slug', 'name' => 'template.slug', 'title' => _i("Template")],
         ];
     }
 
