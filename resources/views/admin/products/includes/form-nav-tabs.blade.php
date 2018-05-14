a0d29415f8a7e5a4e46577f12435426748c66806
Stephane Reynders <spoutnik@gmail.com>
2018-05-14 21:28:22 +0200

    Fix empty modules on product creation

--- resources/views/admin/products/includes/form-nav-tabs.blade.php
+++ resources/views/admin/products/includes/form-nav-tabs.blade.php
@@ -1,12 +1,14 @@
-<ul class="nav nav-tabs mb-2">
-    <li class="nav-item">
-        <a href="{{ route('admin.products.edit', ['id' => $object->id]) }}" class="nav-link {{ request()->route()->getName() === 'admin.products.edit' ? 'active' : '' }}">{{ _i("General") }}</a>
-    </li>
-    @foreach($object->template->modules as $module)
-        @if (in_array($module->pivot->placeholder, $object->template->placeholders))
-            <li class="nav-item">
-                <a href="{{ route('admin.products.edit-module', ['pk' => $object->pk, 'placeholderId' => $module->pivot->id]) }}" class="nav-link {{ isset($placeholderId) && $module->pivot->id == $placeholderId ? 'active' : '' }}">{{ $module->title }} ({{ $module->pivot->title ?? $module->pivot->placeholder }})</a>
-            </li>
-        @endif
-    @endforeach
-</ul>
+@if (!empty($object->template))
+    <ul class="nav nav-tabs mb-2">
+        <li class="nav-item">
+            <a href="{{ route('admin.products.edit', ['id' => $object->id]) }}" class="nav-link {{ request()->route()->getName() === 'admin.products.edit' ? 'active' : '' }}">{{ _i("General") }}</a>
+        </li>
+        @foreach($object->template->modules as $module)
+            @if (in_array($module->pivot->placeholder, $object->template->placeholders))
+                <li class="nav-item">
+                    <a href="{{ route('admin.products.edit-module', ['pk' => $object->pk, 'placeholderId' => $module->pivot->id]) }}" class="nav-link {{ isset($placeholderId) && $module->pivot->id == $placeholderId ? 'active' : '' }}">{{ $module->title }} ({{ $module->pivot->title ?? $module->pivot->placeholder }})</a>
+                </li>
+            @endif
+        @endforeach
+    </ul>
+@endif
