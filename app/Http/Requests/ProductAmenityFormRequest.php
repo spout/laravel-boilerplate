3004703253f6efe29eaca9f741df3016b1e75b57
Stephane Reynders <spoutnik@gmail.com>
2018-05-21 01:40:31 +0200

    Rename product / amenity form request

--- app/Http/Requests/ProductAmenityFormRequest.php
+++ app/Http/Requests/ProductAmenityFormRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class AmenityProductFormRequest extends FormRequest
+class ProductAmenityFormRequest extends FormRequest
 {
     public function authorize()
     {
