49533cb75111295dfd6f2f1163647ebbc1cb08cb
Stephane Reynders <spoutnik@gmail.com>
2018-05-21 20:09:34 +0200

    Add product associated method

--- app/Models/Module.php
+++ app/Models/Module.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\Traits\TranslatableTrait;
+use Illuminate\Support\Collection;
 
 class Module extends Model
 {
@@ -25,5 +26,19 @@ class Module extends Model
     public function templates()
     {
         return $this->belongsToMany(Template::class, 'placeholders')->using(Placeholder::class);
+    }
+
+    /**
+     * @param $productId
+     * @return Collection
+     */
+    public function getProductAssociatedModelInstances($productId)
+    {
+        $moduleModelClass = $this->model_class;
+        $where = [
+            'product_id' => $productId,
+            'placeholder_id' => $this->pivot->id,
+        ];
+        return $moduleModelClass::where($where)->get();
     }
 }
