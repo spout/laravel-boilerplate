4ead3f4fa715963e75ec2f5b4232f347cbcf84f4
Stephane Reynders <spoutnik@gmail.com>
2018-05-22 20:10:47 +0200

    Remove TranslatableTrait from pivot classes (not working)

--- app/Models/ProductAmenity.php
+++ app/Models/ProductAmenity.php
@@ -2,14 +2,10 @@
 
 namespace App\Models;
 
-use App\Models\Traits\TranslatableTrait;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class ProductAmenity extends Pivot
 {
-    use TranslatableTrait;
-
     public $timestamps = false;
     public $guarded = ['amenities'];
-    public static $translatableColumns = ['name'];
 }
--- app/Models/ProductService.php
+++ app/Models/ProductService.php
@@ -2,14 +2,10 @@
 
 namespace App\Models;
 
-use App\Models\Traits\TranslatableTrait;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class ProductService extends Pivot
 {
-    use TranslatableTrait;
-
     public $timestamps = false;
-    public static $translatableColumns = ['name'];
-    public $guarded = ['amenities'];
+    public $guarded = ['services'];
 }
