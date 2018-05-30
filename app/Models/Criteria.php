8638f5bffdde08cc445aa9e6f0a63db29b93860f
Stephane Reynders <spoutnik@gmail.com>
2018-05-30 20:14:07 +0200

    Criteria AdjacencyList

--- app/Models/Criteria.php
+++ app/Models/Criteria.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\Traits\AdjacencyListTrait;
 use App\Models\Traits\TranslatableTrait;
 
 class Criteria extends Model
 {
     use TranslatableTrait;
+    use AdjacencyListTrait;
 
     protected $guarded = [];
     public $timestamps = false;
