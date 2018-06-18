c96513ecd7168932524c58dc3cd5d966c16557a2
spout <spoutnik@gmail.com>
2018-06-18 18:32:40 +0200

    Create products files directory on the fly

--- app/Observers/ProductObserver.php
+++ app/Observers/ProductObserver.php
@@ -20,5 +20,12 @@ class ProductObserver
             $attributes['tag_id'] = $tag;
             Taggable::create($attributes);
         }
+
+        $path = "files/products/{$product->slug_en}/";
+        $dir = public_path($path);
+
+        if (!is_dir($dir)) {
+            mkdir($dir, 0777, true);
+        }
     }
 }
